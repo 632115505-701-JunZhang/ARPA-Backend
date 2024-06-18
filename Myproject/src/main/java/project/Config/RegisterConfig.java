0d0cb693c40937e7673367e24e58036f8f5ae37c
JUN ZHANG <1076353177@qq>
2024-06-18 17:13:54 +0800

    初始化

---
+++ Myproject/src/main/java/project/Config/RegisterConfig.java
@@ -0,0 +1,11 @@
+package project.Config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import project.Controller.RegisterController;
+
+@Configuration
+
+public class RegisterConfig {
+}
